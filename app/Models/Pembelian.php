a0b58d6a771de0985caebf5edef7e49ad5a36f7d
Alvarizqi09 <alvarizki80@students.unnes.ac.id>
2023-04-11 01:41:48 +0700

    model tansaksi

--- app/Models/Pembelian.php
+++ app/Models/Pembelian.php
@@ -6,7 +6,7 @@ use Eloquent;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class pembelian extends Model
+class Pembelian extends Model
 {
     use HasFactory;
     protected $table = 'Pembelians';
---
+++ app/Models/Transaksi.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+use Eloquent;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Transaksi extends Model
+{
+    use HasFactory;
+    protected $table = 'Transaksis';
+    protected $fillable = [
+        'notransaksi', 
+        'tgltransaksi', 
+        'noRM',
+        'namapelanggan',
+        'namaobat',
+        'idobat',
+        'jumlahobat',
+        'totalharga',
+        'status',
+
+    ];
+}
