c300b9b013833d93e341271bdd6da77c0962b64d
Alvarizqi09 <alvarizki80@students.unnes.ac.id>
2023-04-11 01:08:58 +0700

    memperbaiki router pembelian & transaksi

--- routes/web.php
+++ routes/web.php
@@ -35,3 +35,23 @@ Route::resource('resep', resepController::class);
 Route::resource('inventories', InventoryCRUDController::class);
 Route::resource('pembelian', PembelianController::class);
 Route::resource('Transaksi', TransaksiController::class);
+
+Route::get('/transaksi', [TransaksiController::class, 'index'])->name('transaksi');
+
+Route::get('/tambahtransaksi', [TransaksiController::class, 'tambahtransaksi'])->name('tambahtransaksi');
+Route::post('/insertdata', [TransaksiController::class, 'insertdata'])->name('insertdata');
+
+Route::get('/tampilkandata/{id}',[TransaksiController::class, 'tampilkandata'])->name('tampilkandata');
+Route::post('/updatedata/{id}',[TransaksiController::class, 'updatedata'])->name('updatedata');
+
+Route::get('/delete/{id}',[TransaksiController::class, 'delete'])->name('delete');
+
+Route::get('/pembelian', [PembelianController::class, 'index'])->name('transaksi');
+
+Route::get('/tambahtransaksi', [PembelianController::class, 'tambahtransaksi'])->name('tambahtransaksi');
+Route::post('/insertdata', [PembelianController::class, 'insertdata'])->name('insertdata');
+
+Route::get('/tampilkandata/{id}',[PembelianController::class, 'tampilkandata'])->name('tampilkandata');
+Route::post('/updatedata/{id}',[PembelianController::class, 'updatedata'])->name('updatedata');
+
+Route::get('/delete/{id}',[PembelianController::class, 'delete'])->name('delete');
