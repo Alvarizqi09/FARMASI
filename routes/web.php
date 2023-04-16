f61d9bcfe7f3179bf0636fc15e9921f729b14a52
Fariska Ratna <fariskafauziah07@students.unnes.ac.id>
2023-04-16 17:14:45 +0700

    routes

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.dashboard');
 });
 
 Route::resource('stocks', StockCRUDController::class);
