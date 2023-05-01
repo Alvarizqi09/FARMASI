01ef8513452f840420fa5e6b02050bce1a090831
Fariska Ratna <fariskafauziah07@students.unnes.ac.id>
2023-05-01 19:00:06 +0700

    add pengaduan in layout

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -212,6 +212,23 @@
               </li> --}}
             </ul>
           </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-info-circle"></i>
+              <p>
+                Pengaduan
+                <i class="right fas fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="{{ route('pengaduan.index') }}" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Data Pengaduan</p>
+                </a>
+              </li>
+            </ul>
+          </li>
       
           <li class="nav-header">Setting</li>
           <li class="nav-item">
