e5f71619874369c5fb65314cab866e1a38f693c8
susilosuryap <susilosuryap@students.unnes.ac.id>
2023-05-12 17:04:45 +0700

    chanage route

--- routes/web.php
+++ routes/web.php
@@ -35,7 +35,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('layouts.dashboard');
+    return view('layout.main');
 });
 
 Route::resource('stocks', StockCRUDController::class);
