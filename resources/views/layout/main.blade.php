c83f35886c564ca5dea41fd541691c4c24af352f
Fariska Ratna <fariskafauziah07@students.unnes.ac.id>
2023-05-22 20:57:07 +0700

    fix bug overlay scrollbar

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -6,21 +6,33 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Farmasi</title>
 
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
   <link rel="stylesheet" href={{ asset('aset/style.css') }} />
   <!-- Boxiocns CDN Link -->
   <link href="https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css" rel="stylesheet" />
   <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  {{-- <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback"> --}}
   <!-- Font Awesome -->
-  {{-- <link rel="stylesheet" href={{ asset("plugins/fontawesome-free/css/all.min.css") }}> --}}
+  <link rel="stylesheet" href={{ asset("plugins/fontawesome-free/css/all.min.css") }}>
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Tempusdominus Bootstrap 4 -->
+  {{-- <link rel="stylesheet" href="{{ asset(plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css) }}"> --}}
+  <!-- iCheck -->
+  {{-- <link rel="stylesheet" href="{{ asset(plugins/icheck-bootstrap/icheck-bootstrap.min.css) }}"> --}}
+  <!-- JQVMap -->
+  {{-- <link rel="stylesheet" href="{{ asset(plugins/jqvmap/jqvmap.min.css) }}"> --}}
+  <!-- Theme style -->
+  {{-- <link rel="stylesheet" href={{ asset("dist/css/adminlte.min.css") }}> --}}
   <!-- overlayScrollbars -->
   <link rel="stylesheet" href={{ asset("plugins/overlayScrollbars/css/OverlayScrollbars.min.css") }}>
   <link rel="stylesheet" href={{ asset("plugins/datatables/datatables.css") }}>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.3/dist/sweetalert2.min.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
   <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+  <!-- Daterange picker -->
+  {{-- <link rel="stylesheet" href="{{ asset(plugins/daterangepicker/daterangepicker.css) }}"> --}}
+  <!-- summernote -->
+  {{-- <link rel="stylesheet" href="{{ asset(plugins/summernote/summernote-bs4.min.css) }}"> --}}
 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
@@ -118,7 +130,9 @@
           <li>
             <a class="link_name" href="#"> <b>Transaksi</b> </a>
           </li>
-          <li><a href="{{ route('jualans.index') }}">Penjualan Obat</a></li>
+          <li><a href="/jualans">Tambah Penjualan</a></li>
+          <li><a href="{{ route('jualans.home') }}">Laporan Penjualan</a></li>
+          <li><a href="/pembelians">Pembelian Obat</a></li>
         </ul>
       </li>
       <li>
@@ -155,10 +169,10 @@
       <li>
         <div class="profile-details">
           <div class="profile-content">
-            <img src="aset/assets/profile.webp" alt="profileImg" />
+            <img src="aset/assets/lijeong.jpg" alt="profileImg" />
           </div>
           <div class="name-job">
-            <div class="profile_name">Baizhu</div>
+            <div class="profile_name">Lijeong</div>
             <div class="job">Apoteker</div>
           </div>
           <i class="bx bx-log-out"></i>
@@ -196,11 +210,29 @@
 </script>
 <!-- Bootstrap 4 -->
 <script src={{ asset("plugins/bootstrap/js/bootstrap.bundle.min.js") }}></script>
+<!-- ChartJS -->
+{{-- <script src={{ asset("plugins/chart.js/Chart.min.js") }}></script> --}}
+<!-- Sparkline -->
+{{-- <script src={{ asset("plugins/sparklines/sparkline.js") }}></script> --}}
+<!-- JQVMap -->
+{{-- <script src={{ asset("plugins/jqvmap/jquery.vmap.min.js") }}></script> --}}
+{{-- <script src={{ asset("plugins/jqvmap/maps/jquery.vmap.usa.js") }}></script> --}}
+<!-- jQuery Knob Chart -->
+{{-- <script src={{ asset("plugins/jquery-knob/jquery.knob.min.js") }}></script> --}}
+<!-- daterangepicker -->
+{{-- <script src={{ asset("plugins/moment/moment.min.js") }}></script> --}}
+{{-- <script src={{ asset("plugins/daterangepicker/daterangepicker.js") }}></script> --}}
+<!-- Tempusdominus Bootstrap 4 -->
+{{-- <script src={{ asset("plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js") }}></script> --}}
+<!-- Summernote -->
+{{-- <script src={{ asset("plugins/summernote/summernote-bs4.min.js") }}></script> --}}
 <!-- overlayScrollbars -->
 <script src={{ asset("plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js") }}></script>
 <!-- AdminLTE App -->
 <script src={{ asset("dist/js/adminlte.js") }}></script>
 <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+{{-- <script src={{ asset("dist/js/demo.js") }}></script> --}}
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
 {{-- <script src={{ asset("dist/js/pages/dashboard.js") }}></script> --}}
 <script type="text/javascript">
@@ -227,5 +259,6 @@
 </script>
 <script src="https://unpkg.com/bootstrap-datepicker@1.9.0/dist/js/bootstrap-datepicker.min.js"></script>
 <script src={{ asset("plugins/datatables/jquery.dataTables.js") }}></script>
+<script src="http://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 </body>
 </html>
