25de3d44c2691c3e5531261cd39fe1372e58b544
Alvarizqi09 <alvarizki80@students.unnes.ac.id>
2023-05-28 01:32:15 +0700

    google

--- config/app.php
+++ config/app.php
@@ -182,7 +182,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Laravel\Socialite\SocialiteServiceProvide::class,
-        Laravel\Socialite\Facades\Socialite::class,
+        
         /*
          * Package Service Providers...
          */
