b719a70ac4e138aa91fd784609eb48ca03b6ca24
Alvarizqi09 <alvarizki80@students.unnes.ac.id>
2023-05-28 01:12:11 +0700

    login google

--- app/Http/Controllers/GoogleController.php
+++ app/Http/Controllers/GoogleController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Exception;
+use Laravel\Socialite\Facades\Socialite;
 use Illuminate\Http\Request;
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Laravel\Socialite\Facades\Socialite;
+
 
 class GoogleController extends Controller
 {
