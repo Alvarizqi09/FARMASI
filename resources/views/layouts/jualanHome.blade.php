b10fcdbe0891a459a7a69a3fbae2686b6e8b9e0e
Fariska Ratna <fariskafauziah07@students.unnes.ac.id>
2023-06-03 22:58:51 +0700

    select2

--- resources/views/layouts/jualanHome.blade.php
+++ resources/views/layouts/jualanHome.blade.php
@@ -195,7 +195,7 @@
     <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.3/dist/sweetalert2.all.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js" defer></script>
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
     {{-- <script src={{ asset("plugins/datatables/jquery.dataTables.js") }}></script> --}}
 
     <script>
